c3f2978035322bd33efebd9c656e002905215647
Dora Yihong <694612150@qq.com>
2019-12-04 20:47:01 +0800

    1004

--- 1004-2.py
+++ 1004-2.py
@@ -1,11 +1,14 @@
-def dfs(node, degree):
+def dfs(node, depth):
+    global max_depth
+    max_depth = max(max_depth, depth)
+    print(node, depth)
     is_visited[node] = 1
     if tree.get(node, -1) == -1:  # 没有子节点
-        level[degree] += 1
+        level[depth] += 1
         return
     for child in tree[node]:  # dfs遍历当前节点的所有子节点
         if is_visited[child] == 0:
-            dfs(child, degree + 1)  # 递归
+            dfs(child, depth + 1)  # 递归
 
 
 # n:树中节点数
@@ -15,6 +18,7 @@ if n == 1:  # 只有一个节点直接输出1
     print("1")
 else:
     tree = {}  # 树
+    max_depth = 0
     level = [0] * (n+1)  #
     is_visited = [0] * (n+1)  # 是否已访问
     # 初始化树
@@ -29,15 +33,6 @@ else:
     # print("child_id:", child_id)
     print("tree:", tree)
     print("level:", level)
-
-    num_leaf_node = n - m  # 叶子节点数
-    count = 0
-    result = []
-    for i in level:
-        result.append(i)
-        count += i
-        if count >= num_leaf_node:
-            break
-    print("result:", result)
+    print("max_depth:", max_depth)
     print("is_visited", is_visited)
-    print(" ".join(str(i) for i in result))
+    print(" ".join(str(i) for i in level[0:max_depth+1]))
