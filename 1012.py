93ed85a00e008a0af695a8eaa587917e51f70226
Dora Yihong <694612150@qq.com>
2019-12-06 21:27:24 +0800

    1012

---
+++ 1012.py
@@ -0,0 +1,40 @@
+def rank(num, nums):
+    array = list(nums)
+    rank_num = 1
+    while num != max(array):
+        del array[array.index(max(array))]
+        rank_num += 1
+    return rank_num
+
+
+def output(s_id):
+    pos = sid.index(s_id)
+    temp = analyse[pos]
+    pos = temp.index(min(temp))
+    print(min(temp), acme[pos])
+
+
+n, m = list(map(int, input().split()))
+acme = ['A', 'C', 'M', 'E']
+sid, score_c, score_m, score_e, score_a = [], [], [], [], []
+analyse, input_sid = [], []
+for i in range(n):
+    x = input().split()
+    sid.append(x[0])
+    score_c.append(int(x[1]))
+    score_m.append(int(x[2]))
+    score_e.append(int(x[3]))
+    score_a.append((int(x[1]) + int(x[2]) + int(x[3])) / 3)
+
+for i in range(n):
+    analyse.append(
+        [rank(score_a[i], score_a), rank(score_c[i], score_c), rank(score_m[i], score_m), rank(score_e[i], score_e)])
+
+for i in range(m):
+    input_sid.append(input())
+
+for i in range(m):
+    if input_sid[i] in sid:
+        output(input_sid[i])
+    else:
+        print("N/A")
